fcffd2d1c1a9de625829f8f04606fce96f8fec55
Tasty-Pie <kuanysh.drk@gmail.com>
2023-05-16 14:36:46 +0600

    Added Game Controller class to detect collisions, restart the game or spawn a new block accordingly. Added a tag for the platform named "Platform".

--- Assets/Scripts/GameplayController.cs
+++ Assets/Scripts/GameplayController.cs
@@ -6,15 +6,61 @@ public class GameplayController : MonoBehaviour
 {
     public static GameplayController instance;
 
+    public BlockSpawner blockSpawner;
+
+    [HideInInspector]
+    public BlockScript currentBlock;
+
+    public CameraFollow cameraScript;
+    private int moveCount;
+
     void Awake()
     {
+        instance = this;
     }
 
     void Start()
     {
+        blockSpawner.SpawnBlock();
     }
 
     void Update()
     {
+        ParseInput();
+    }
+
+    void ParseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentBlock.DropBlock();
+        }
+    }
+
+    public void SpawnNewBlock()
+    {
+        Invoke("NewBlock", 1.0f);
+    }
+
+    private void NewBlock()
+    {
+        blockSpawner.SpawnBlock();
+    }
+
+    public void MoveCamera()
+    {
+        moveCount++;
+        if (moveCount == 3)
+        {
+            moveCount = 0;
+            //cameraScript.targetPos.y += 2.0f;
+        }
+    }
+
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetActiveScene().name
+            );
     }
 } // CLASS END
